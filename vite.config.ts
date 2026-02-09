afd2e23cc9ba2ae73f5bd06c096f46dd3b736e73
dev-luk <164710623+dev-luk@users.noreply.github.com>
2026-02-08 23:15:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { HashRouter } from "react-router-dom";
+
+<HashRouter>
+  {/* suas rotas */}
+</HashRouter>
+
 
 export default defineConfig({
   plugins: [react()],
