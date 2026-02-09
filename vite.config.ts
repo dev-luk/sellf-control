4354e17ccf333e034260aaa9976516fdd4446fec
dev-luk <164710623+dev-luk@users.noreply.github.com>
2026-02-08 23:20:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { HashRouter } from "react-router-dom";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-<HashRouter>
-  {/* suas rotas */}
-</HashRouter>
-
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/sellf-control/",
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
